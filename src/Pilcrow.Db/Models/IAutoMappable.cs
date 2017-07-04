afbc3260bcf6bcd8775722a8454d82602efd53fe
Erik Edlund <erik.edlund@32767.se>
2017-07-04 23:01:00 +0200

    Update documentation.

--- src/Pilcrow.Db/Models/IAutoMappable.cs
+++ src/Pilcrow.Db/Models/IAutoMappable.cs
@@ -2,9 +2,9 @@
 namespace Pilcrow.Db.Models
 {
     /// <summary>
-    /// Have models implement this interface if they can not be
-    /// AutoMapped by MongoDb.Driver. In that case the mapping
-    /// must be handled manually during Startup.
+    /// Have models implement this interface if they can be
+    /// AutoMapped by MongoDb.Driver. If this is not an option,
+    /// the mapping must be handled manually during Startup.
     /// </summary>
     public interface IAutoMappable
     {
